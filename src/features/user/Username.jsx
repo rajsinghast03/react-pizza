eed69d662f03252518dd4bb64a191501730afe82
Raj Singh <rajsinghast03@gmail.com>
2023-12-15 21:51:24 +0530

    Initial Commit

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,12 @@
+import { getUsername } from "./userSlice";
+import { useSelector } from "react-redux";
+
+function Username() {
+  const username = useSelector(getUsername);
+  if (!username) return null;
+  return (
+    <div className="hidden text-sm font-semibold md:block">{username}</div>
+  );
+}
+
+export default Username;
